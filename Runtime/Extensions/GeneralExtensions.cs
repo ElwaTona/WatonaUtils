dcc07a2b25e529d61f3017cfc3e8500664539b9f
Watona <elwa.bazaesg@alumnos.arcos.cl>
2022-09-17 05:16:00 -0300

    Hope this lets me use extensions

--- Runtime/Extensions/GeneralExtensions.cs
+++ Runtime/Extensions/GeneralExtensions.cs
@@ -5,7 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using UnityEngine;
 
-namespace Watona.Utils
+namespace Watona.Utils.Extensions
 {
     public static class GeneralExtensions
     {
--- Runtime/Extensions/ToolkitExtensions.cs
+++ Runtime/Extensions/ToolkitExtensions.cs
@@ -2,28 +2,24 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UIElements;
-using Watona;
 
-namespace Watona.Utils
+namespace Watona.Utils.Extensions
 {
-    public static class ToolkitUtils
+    public static class ToolkitExtensions
     {
-        public static VisualElement ChangeFiller(this VisualElement filler, float current, float max)
-        {
-            float percentage = (current/max) * 100;
-            if (percentage > 100)
-                percentage = 100;
-            filler.style.width = new StyleLength(Length.Percent(percentage));
-            return filler;
-
-        }
-
         public static VisualElement SetPosition(this VisualElement bar, Transform transformToFollow, Camera mainCamera)
         {
             Vector2 newPosition = RuntimePanelUtils.CameraTransformWorldToPanel(bar.panel, transformToFollow.position, mainCamera);
 
             bar.transform.position = newPosition.WithNewX(newPosition.x - bar.layout.width / 2);
             return bar;
+        }
+        public static VisualElement ChangeFiller(this VisualElement filler, float current, float max)
+        {
+            float percentage = 0;
+            percentage = percentage >= 100 ? 100 : (current/max) * 100;
+            filler.style.width = new StyleLength(Length.Percent(percentage));
+            return filler;
         }
     }
 }
