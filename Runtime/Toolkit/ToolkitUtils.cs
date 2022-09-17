d73f71a0f105ad0a60d7f3dc2d3be7f04cb0e8a1
Watona <elwa.bazaesg@alumnos.arcos.cl>
2022-09-16 21:52:58 -0300

    Change Position UI

--- Runtime/Toolkit/ToolkitUtils.cs
+++ Runtime/Toolkit/ToolkitUtils.cs
@@ -2,10 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UIElements;
-
+using Watona.Utils;
 namespace Watona.Utils.Toolkit
 {
-    public class ToolkitUtils
+    public static class ToolkitUtils
     {
         public static void ChangeFiller(VisualElement filler, float current, float max)
         {
@@ -13,6 +13,13 @@ namespace Watona.Utils.Toolkit
             if (percentage > 100)
                 percentage = 100;
             filler.style.width = new StyleLength(Length.Percent(percentage));
+        }
+
+        public static void SetPosition(VisualElement bar, Transform transformToFollow, Camera mainCamera)
+        {
+            Vector2 newPosition = RuntimePanelUtils.CameraTransformWorldToPanel(bar.panel, transformToFollow.position, mainCamera);
+
+            bar.transform.position = newPosition.WithNewX(newPosition.WithNewX = bar.layout.widht / 2);
         }
     }
 }
