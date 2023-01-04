1eacc4ceb1bc64df200a3894976a1d72f4e60fb3
Watona <elwa.bazaesg@alumnos.arcos.cl>
2023-01-04 18:43:52 -0300

    Update

--- Runtime/Extensions/GeneralExtensions.cs
+++ Runtime/Extensions/GeneralExtensions.cs
@@ -20,5 +20,25 @@ namespace Watona.Utils.Extensions
         /// Returns the current vector with a new X
         /// </summary>
         public static Vector2 WithNewX(this Vector2 vector, float newX) => new Vector2(newX, vector.y);
+        
+        ///<summary> Returns the array including the new item </summary>
+        public static T[] With<T>(this T[] array, T item)
+        {
+            Array.Resize(ref array, array.Length + 1);
+            array[array.Length - 1] = item;
+            return array;
+        }
+        ///<summary> Returns the array without the new item </summary>
+        public static T[] Without<T>(this T[] array, T item)
+        {
+            for (int i = Array.IndexOf(array, item); i < array.Length - 1; i++)
+                {
+                    // moving elements downwards, to fill the gap at [index]
+                    array[i] = array[i + 1];
+                }
+            // finally, let's decrement Array's size by one
+            Array.Resize(ref array, array.Length - 1);
+            return array;
+        }
     }
 }
